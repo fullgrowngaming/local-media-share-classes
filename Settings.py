b52ac8a1459f5f2f23d737c2ffdf9d2fecb1ce0a
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2018-10-03 02:24:09 -0700

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,5 +1,5 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
-PASS = 'oauth:zi6iddj0job0p3rpnwnxblzp392jje'
+PASS = 'oauth:zi6iddj0job0p3rpnwnxblzp392jje' #this no longer works
 NICK = 'kylfbot'
 CHANNEL = 'fullgrowngaming'
