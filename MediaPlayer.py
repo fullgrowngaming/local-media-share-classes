34d17f65d26b3d56645e967182839180ea6ae9ea
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2018-10-05 13:51:02 -0700

    Add files via upload

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -7,29 +7,24 @@ class MediaPlayer:
         self.instance = vlc.Instance()
         self.player = self.instance.media_player_new()
 
-    def construct_url(self, url):
+    def play_video(self, duration, url):
+
         if url == None:
             return None
-        
-        video = pafy.new(url)
-        best = video.getbest()
-        return best.url
 
-    def play_video(self, duration, url):
-        parsed_url = self.construct_url(url)
+        parsed_url = pafy.new(url).getbest().url
 
-        if parsed_url != None:
-            media = self.instance.media_new(parsed_url)
-            media.get_mrl()
-            self.player.set_media(media)
-            self.player.play()
+        media = self.instance.media_new(parsed_url)
+        media.get_mrl()
+        self.player.set_media(media)
+        self.player.play()
     
-            end = time.time() + duration
+        end = time.time() + duration
     
-            while True:
-                if time.time() > end:
-                    self.player.stop()
-                    break
-                if input() == 'skip':
-                    self.player.stop()
-                    break
+        while True:
+            if time.time() > end:
+                self.player.stop()
+                break
+            if input() == 'skip':
+                self.player.stop()
+                break
